d2f2976ddc77a2689bebe718ce8403b39a12fee8
evelyn-plee <evelyn.plee@gmail.com>
2019-02-14 21:08:03 +1300

    test: added unit tests for base validator, lt

--- src/baseValidators/lt/index.spec.js
+++ src/baseValidators/lt/index.spec.js
@@ -3,16 +3,29 @@ import { Just, Left, Right, Nothing } from 'sanctuary'
 import lt from './'
 
 describe('baseValidators:lt', () => {
-  it(`returns Right(Just(24)) for Just(12)`, () => {
-    expect(lt(Just(13))(Just(12))).toEqual(Right(Just(12)))
+  it(`returns Right(Just(11)) for lt(Just(12))(Just(11))`, () => {
+    expect(lt(Just(12))(Just(11))).toEqual(Right(Just(11)))
   })
 
-  //it(`returns Left(Just(11)) for Just(12)`, () =>{
-  //    expect(lt(Just(11))(Just(12))).toMatchObject(
-  //            Left({
-  //            failures: [{ errorType: 'FAIL_1', testValue: Just(5) }],
-  //            value: Just(1)
-  //  })
-  //)
-  //})
+  it(`Left ({"failures": [{"errorType": "NOT_LT", "testValue": Just (12)}], "value": Just (12)})`, () => {
+    expect(lt(Just(12))(Just(12))).toEqual(
+      Left({
+        failures: [{ errorType: 'NOT_LT', testValue: Just(12) }],
+        value: Just(12)
+      })
+    )
+  })
+
+  it(`Left ({"failures": [{"errorType": "NOT_LT", "testValue": Just (12)}], "value": Just (13)})`, () => {
+    expect(lt(Just(12))(Just(13))).toEqual(
+      Left({
+        failures: [{ errorType: 'NOT_LT', testValue: Just(12) }],
+        value: Just(13)
+      })
+    )
+  })
+
+  it(`returns Right(Nothing) for lt(Just(12))(Nothing)`, () => {
+    expect(lt(Just(12))(Nothing)).toEqual(Right(Nothing))
+  })
 })
