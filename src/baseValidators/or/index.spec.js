b22381fbf444d00b9546e11e6d5443b638d3e364
chasm <chas@munat.com>
2019-02-18 09:32:57 +1300

    test: update test descriptions

--- src/baseValidators/or/index.spec.js
+++ src/baseValidators/or/index.spec.js
@@ -1,4 +1,4 @@
-import { Just, Nothing, Left, Right } from 'sanctuary'
+import { Just, Left, Nothing, Right } from 'sanctuary'
 
 import createFailures from '../../utilities/createFailures'
 import or from './'
@@ -13,23 +13,23 @@ describe('baseValidators:or', () => {
     expect(or([success, success])()).toEqual(Right(Nothing))
   })
 
-  it(`returns Right(${value} for success OR success)`, () => {
+  it(`returns Right(value) for success OR success)`, () => {
     expect(or([success, success])(value)).toEqual(Right(value))
   })
 
-  it(`returns Right(${value} for success OR success OR success`, () => {
+  it(`returns Right(value) for success OR success OR success`, () => {
     expect(or([success, success, success])(value)).toEqual(Right(value))
   })
 
-  it(`returns Right(${value} for failure('x') OR success`, () => {
+  it(`returns Right(value) for failure OR success`, () => {
     expect(or([failure('x'), success])(value)).toEqual(Right(value))
   })
 
-  it(`returns Right(${value} for success or failure('y')`, () => {
+  it(`returns Right(value) for success or failure`, () => {
     expect(or([success, failure('y')])(value)).toEqual(Right(value))
   })
 
-  it(`returns Left({failures: [{ errorType: 'FAILURE_x', testValue: Just('test') },{ errorType: 'FAILURE_y', testValue: Just('test') }], value: Just(1)}) for failure('x') OR failure('y')`, () => {
+  it(`returns Left(combined errors) for failure OR failure`, () => {
     expect(or([failure('x'), failure('y')])(value)).toEqual(
       Left({
         failures: [
@@ -41,7 +41,7 @@ describe('baseValidators:or', () => {
     )
   })
 
-  it(`returns Left({failures: [{ errorType: 'FAILURE_x', testValue: Just('test') },{ errorType: 'FAILURE_y', testValue: Just('test') }], value: Just(1)}) for failure('x') OR failure('y') OR failure('x')`, () => {
+  it(`returns Left(combined unique errors) for failure or failure or failure with duplication`, () => {
     expect(or([failure('x'), failure('y'), failure('x')])(value)).toEqual(
       Left({
         failures: [
