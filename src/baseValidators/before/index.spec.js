c8058a0ad6386d31e9b7336f6cfb353cc5bb47f7
chasm <chas@munat.com>
2019-02-18 08:16:30 +1300

    test: simplify test descriptions

--- src/baseValidators/before/index.spec.js
+++ src/baseValidators/before/index.spec.js
@@ -4,15 +4,13 @@ import { IS_NULL } from '../../errorTypes'
 import before from './'
 
 describe('baseValidators:before', () => {
-  it(`returns Right (Just (new Date ("2000-12-31T00:00:00.000Z")))) 
-    for before(parseDate('2001-01-01'))(parseDate('2000-12-31'))`, () => {
+  it(`returns Right(Just(value))) when value is before testDate`, () => {
     expect(before(parseDate('2001-01-01'))(parseDate('2000-12-31'))).toEqual(
       Right(Just(new Date('2000-12-31T00:00:00.000Z')))
     )
   })
 
-  it(`returns Left ({"failures": [{"errorType": "NOT_BEFORE_DATE_TIME", "testValue": Just (new Date ("2001-01-01T00:00:00.000Z"))}], "value": Just (new Date ("2001-01-01T00:00:00.000Z"))})
-    for before(parseDate('2001-01-01'))(parseDate('2001-01-01'))`, () => {
+  it(`returns Left(NOT_BEFORE_DATE_TIME error) when value is the same as testDate`, () => {
     expect(before(parseDate('2001-01-01'))(parseDate('2001-01-01'))).toEqual(
       Left({
         failures: [
@@ -26,8 +24,7 @@ describe('baseValidators:before', () => {
     )
   })
 
-  it(`returns Left ({"failures": [{"errorType": "NOT_BEFORE_DATE_TIME", "testValue": Just (new Date ("2001-01-01T00:00:00.000Z"))}], "value": Just (new Date ("2001-01-02T00:00:00.000Z"))})
-    for before(parseDate('2001-01-01'))(parseDate('2001-01-02'))`, () => {
+  it(`returns Left(NOT_BEFORE_DATE_TIME error) when value is after testDate`, () => {
     expect(before(parseDate('2001-01-01'))(parseDate('2001-01-02'))).toEqual(
       Left({
         failures: [
@@ -41,8 +38,7 @@ describe('baseValidators:before', () => {
     )
   })
 
-  it(`returns Right (Nothing)
-    for before(parseDate('2001-01-01'))(Nothing)`, () => {
+  it(`returns Right(Nothing) when value is Nothing`, () => {
     expect(before(parseDate('2001-01-01'))(Nothing)).toEqual(Right(Nothing))
   })
 })
