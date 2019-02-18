6643d6fb71b066b7dd0f3ecd8e08807ac7af4dcf
Matt Norris <matthew.norris96@gmail.com>
2019-02-18 21:56:44 +1300

    test: added unit tests for gtechars

---
+++ src/fieldValidators/string/gteChars/index.spec.js
@@ -0,0 +1,32 @@
+import { Just, Left, Nothing, Right } from 'sanctuary'
+
+import { TOO_FEW_CHARACTERS } from '../../../errorTypes'
+import gteChars from './'
+
+describe('fieldValidators:gteChars', () => {
+  it(`returns Left(Just(value)) when value is less than testValue`, () => {
+    expect(gteChars(Just(5))(Just('Bob'))).toEqual(
+      Left({
+        failures: [
+          {
+            errorType: TOO_FEW_CHARACTERS,
+            testValue: Just(5)
+          }
+        ],
+        value: Just('Bob')
+      })
+    )
+  })
+
+  it(`returns Right(Just(value)) when value is more than testValue`, () => {
+    expect(gteChars(Just(5))(Just('Tommy'))).toEqual(Right(Just('Tommy')))
+  })
+
+  it(`returns Right(Nothing) when value is nothing`, () => {
+    expect(gteChars(Just(5))(Nothing)).toEqual(Right(Nothing))
+  })
+
+  it(`returns Right(Just(value)) when the testValue is nothing`, () => {
+    expect(gteChars(Nothing(Just('Tommy'))).toEqual(Right('Tommy')))
+  })
+})
