3b5899087d9fee74d0a592745842756251216881
Matt Norris <matthew.norris96@gmail.com>
2019-02-18 16:44:55 +1300

    test: fixed unit tests for equalTo

--- src/baseValidators/equalTo/index.spec.js
+++ src/baseValidators/equalTo/index.spec.js
@@ -7,12 +7,13 @@ describe('baseValidators:equalTo', () => {
     expect(equalTo(Just(20))(Just(20))).toEqual(Right(Just(20)))
   })
 
-  it(`returns Left(Just(value)) when value is less than testValue`, () => {
+  it(`returns Left(Just(value)) when value is more than testValue`, () => {
     expect(equalTo(Just(19))(Just(20))).toEqual(
       Left({
         failures: [
           {
-            errorType: NOT_EQUAL_TO
+            errorType: NOT_EQUAL_TO,
+            testValue: Just(19)
           }
         ],
         value: Just(20)
@@ -20,12 +21,13 @@ describe('baseValidators:equalTo', () => {
     )
   })
 
-  it(`returns Left(Just(value)) when value is more than testValue`, () => {
+  it(`returns Left(Just(value)) when value is less than testValue`, () => {
     expect(equalTo(Just(21))(Just(20))).toEqual(
       Left({
         failures: [
           {
-            errorType: NOT_EQUAL_TO
+            errorType: NOT_EQUAL_TO,
+            testValue: Just(21)
           }
         ],
         value: Just(20)
@@ -34,6 +36,6 @@ describe('baseValidators:equalTo', () => {
   })
 
   it(`returns Right(Nothing) when value is Nothing`, () => {
-    expect(equalTo(Just(21))(Just(20))).toEqual(Right(Nothing))
+    expect(equalTo(Just(20))(Nothing)).toEqual(Right(Nothing))
   })
 })
