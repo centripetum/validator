d99ac0b54e1511c9db31cf7811f7495d8370695c
evelyn-plee <evelyn.plee@gmail.com>
2019-02-19 20:21:07 +1300

    test: updated descriptions to be less technical

--- src/fieldValidators/string/gtChars/index.spec.js
+++ src/fieldValidators/string/gtChars/index.spec.js
@@ -3,7 +3,7 @@ import { Just, Left, Right, Nothing } from 'sanctuary'
 import gtChars from './'
 
 describe('fieldValidators:string:gtChars', () => {
-  it(`Left ({"failures": [{"errorType": "TOO_FEW_CHARACTERS", "testValue": Just(3)}], "value": Just ("Bob")})`, () => {
+  it(`returns Left(TOO_FEW_CHARACTERS error) when the length of the string is the same as the testValue`, () => {
     expect(gtChars(Just(3))(Just('Bob'))).toEqual(
       Left({
         failures: [{ errorType: 'TOO_FEW_CHARACTERS', testValue: Just(3) }],
@@ -12,15 +12,15 @@ describe('fieldValidators:string:gtChars', () => {
     )
   })
 
-  it(`returns Right(Just("Tommy")) for gtChars(Just(3))(Just("Tommy"))`, () => {
+  it(`returns Right(Just(value)) when the length of the string is greater than the testValue`, () => {
     expect(gtChars(Just(3))(Just('Tommy'))).toEqual(Right(Just('Tommy')))
   })
 
-  it(`returns Right(Nothing) for gtChars(Just(3))(Nothing)`, () => {
+  it(`returns Right(Nothing) when value is Nothing`, () => {
     expect(gtChars(Just(3))(Nothing)).toEqual(Right(Nothing))
   })
 
-  it(`returns Right(Just("Tommy")) for gtChars(Nothing)(Just("Tommy"))`, () => {
+  it(`returns Right(Just(value)) when the testValue is Nothing`, () => {
     expect(gtChars(Nothing)(Just('Tommy'))).toEqual(Right(Just('Tommy')))
   })
 })
