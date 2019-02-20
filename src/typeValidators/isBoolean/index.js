3d89dcd125c4dddb564dd6492a17a0fffbd93d58
CliffRobinson <Clifford.F.Robinson@gmail.com>
2019-02-20 17:17:13 +1300

    test:isBoolean validator and tests

---
+++ src/typeValidators/isBoolean/index.js
@@ -0,0 +1,22 @@
+// @flow
+
+import {
+  Either,
+  Left,
+  Maybe,
+  Right,
+  is,
+  isJust,
+  maybeToNullable
+} from 'sanctuary'
+
+import $ from 'sanctuary-def'
+
+import { NOT_A_BOOLEAN } from '../../errorTypes'
+
+import createFailures from '../../utilities/createFailures'
+
+export default (value: Maybe<Boolean>): Either<Failures, Maybe<Boolean>> =>
+  isJust(value) && is($.Boolean)(maybeToNullable(value))
+    ? Right(value)
+    : Left(createFailures(NOT_A_BOOLEAN, value))
