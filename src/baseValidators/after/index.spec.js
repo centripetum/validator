198108a87555331586c473ea321f30a7ee125a9c
Wanja Leuthold <wanja.leuthold@gmail.com>
2019-02-20 19:49:27 +1300

    test: clean up tests so they are consistent with the rest

--- src/baseValidators/after/index.spec.js
+++ src/baseValidators/after/index.spec.js
@@ -3,25 +3,25 @@ import { Left, Nothing, parseDate, Right } from 'sanctuary'
 import { NOT_AFTER_DATE_TIME } from '../../errorTypes'
 import after from './'
 
-const testDate = parseDate('2001-01-01T00:00:00.000Z')
-
 describe('baseValidators:after', () => {
   it(`returns Left (NOT_AFTER_DATE_TIME, testValue, value) if the value date-time is before the testValue date-time`, () => {
-    expect(after(testDate)(parseDate('2000-12-31'))).toEqual(
+    expect(after(parseDate('2001-01-01'))(parseDate('2000-12-31'))).toEqual(
       Left({
-        failures: [{ errorType: NOT_AFTER_DATE_TIME, testValue: testDate }],
+        failures: [
+          { errorType: NOT_AFTER_DATE_TIME, testValue: parseDate('2001-01-01') }
+        ],
         value: parseDate('2000-12-31')
       })
     )
   })
 
   it(`returns Left (NOT_AFTER_DATE_TIME, testValue, value) if the value date-time is on the testValue date-time`, () => {
-    expect(after(testDate)(parseDate('2001-01-01'))).toEqual(
+    expect(after(parseDate('2001-01-01'))(parseDate('2001-01-01'))).toEqual(
       Left({
         failures: [
           {
             errorType: NOT_AFTER_DATE_TIME,
-            testValue: testDate
+            testValue: parseDate('2001-01-01')
           }
         ],
         value: parseDate('2001-01-01')
@@ -30,12 +30,12 @@ describe('baseValidators:after', () => {
   })
 
   it(`returns Right (value) if the value date-time is after the testValue date-time`, () => {
-    expect(after(testDate)(parseDate('2001-01-02'))).toEqual(
+    expect(after(parseDate('2001-01-01'))(parseDate('2001-01-02'))).toEqual(
       Right(parseDate('2001-01-02'))
     )
   })
 
   it(`returns Right(Nothing) by default`, () => {
-    expect(after(testDate)(Nothing)).toEqual(Right(Nothing))
+    expect(after(parseDate('2001-01-01'))(Nothing)).toEqual(Right(Nothing))
   })
 })
