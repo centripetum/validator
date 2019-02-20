5b4a3aebe1615d7513c0391e9a458d9185182dbd
Wanja Leuthold <wanja.leuthold@gmail.com>
2019-02-20 20:33:34 +1300

    test: update test descriptions to be consistent with the rest

--- src/baseValidators/onOrBefore/index.spec.js
+++ src/baseValidators/onOrBefore/index.spec.js
@@ -6,19 +6,19 @@ import onOrBefore from './'
 const testDate = parseDate('2001-01-01T00:00:00.000Z')
 
 describe('baseValidators:onOrAfter', () => {
-  it(`returns Left(value) if the value date-time is before the testValue date-time`, () => {
+  it(`returns Right(Just(value)) if the value date-time is before the testValue date-time`, () => {
     expect(onOrBefore(testDate)(parseDate('2000-12-31'))).toEqual(
       Right(parseDate('2000-12-31'))
     )
   })
 
-  it(`returns Right(value) if the value date-time is on the testValue date-time`, () => {
+  it(`returns Right(Just(value)) if the value date-time matches the testValue date-time`, () => {
     expect(onOrBefore(testDate)(parseDate('2001-01-01'))).toEqual(
       Right(parseDate('2001-01-01'))
     )
   })
 
-  it(`returns Left(NOT_ON_OR_BEFORE_DATE_TIME, testValue, value) if the value date-time is after the testValue date-time`, () => {
+  it(`returns Left(NOT_ON_OR_BEFORE_DATE_TIME error) if the value date-time is after the testValue date-time`, () => {
     expect(onOrBefore(testDate)(parseDate('2001-01-02'))).toEqual(
       Left({
         failures: [
