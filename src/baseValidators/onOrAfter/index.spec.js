260364fbdabb73ab05c9fbb9b894b9c27a9c53b8
Wanja Leuthold <wanja.leuthold@gmail.com>
2019-02-20 20:37:28 +1300

    test: update test descriptions to be consistent with the rest

--- src/baseValidators/onOrAfter/index.spec.js
+++ src/baseValidators/onOrAfter/index.spec.js
@@ -6,7 +6,7 @@ import onOrAfter from './'
 const testDate = parseDate('2001-01-01T00:00:00.000Z')
 
 describe('baseValidators:onOrAfter', () => {
-  it(`returns Left(NOT_ON_OR_AFTER_DATE_TIME, testValue, value) if the value date-time is before the testValue date-time`, () => {
+  it(`returns Left(NOT_ON_OR_AFTER_DATE_TIME error) if the value date-time is before the testValue date-time`, () => {
     expect(onOrAfter(testDate)(parseDate('2000-12-31'))).toEqual(
       Left({
         failures: [
@@ -17,13 +17,13 @@ describe('baseValidators:onOrAfter', () => {
     )
   })
 
-  it(`returns Right(value) if the value date-time is on the testValue date-time`, () => {
+  it(`returns Right(Just(value)) if the value date-time matches the testValue date-time`, () => {
     expect(onOrAfter(testDate)(parseDate('2001-01-01'))).toEqual(
       Right(parseDate('2001-01-01'))
     )
   })
 
-  it(`returns Right(value) if the value date-time is after the testValue date-time`, () => {
+  it(`returns Right(Just(value)) if the value date-time is after the testValue date-time`, () => {
     expect(onOrAfter(testDate)(parseDate('2001-01-02'))).toEqual(
       Right(parseDate('2001-01-02'))
     )
