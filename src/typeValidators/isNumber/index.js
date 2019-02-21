375c0b69aaf6b7e51f86a74012679377a923906d
Wanja Leuthold <wanja.leuthold@gmail.com>
2019-02-21 18:01:57 +1300

    feat: set up files

---
+++ src/typeValidators/isNumber/index.js
@@ -0,0 +1,25 @@
+// @flow
+
+import {
+  Either,
+  Left,
+  Maybe,
+  Nothing,
+  Right,
+  is,
+  isJust,
+  maybeToNullable
+} from 'sanctuary'
+
+import $ from 'sanctuary-def'
+
+import { NOT_A_STRING } from '../../errorTypes'
+
+import createFailures from '../../utilities/createFailures'
+
+// copied from the isString validator
+
+// export default (value: Maybe<string>): Either<Failures, Maybe<string>> =>
+//     isJust(value) && is($.String)(maybeToNullable(value))
+//         ? Right(value)
+//         : Left(createFailures(NOT_A_STRING, value))
