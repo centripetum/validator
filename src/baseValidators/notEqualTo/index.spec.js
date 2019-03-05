d8947a1475f7619c8776f521754f534835922031
chasm <chas@munat.com>
2019-03-05 18:42:24 +1300

    refactor: update the test descriptions for notUnequalTo

--- src/baseValidators/notEqualTo/index.spec.js
+++ src/baseValidators/notEqualTo/index.spec.js
@@ -4,11 +4,11 @@ import { NOT_UNEQUAL_TO } from '../../errorTypes'
 import notEqualTo from './'
 
 describe('baseValidators:notEqualTo', () => {
-  it(`returns Right(Just(19)) for (Just(20))(Just(19))`, () => {
+  it(`returns Right(Just(value)) when the value is less than the test value`, () => {
     expect(notEqualTo(Just(20))(Just(19))).toEqual(Right(Just(19)))
   })
 
-  it(`returns Left({"failures": [{"errorType": undefined, "testValue": Just (20)}], "value": Just (20)}) for Just(20))(Just(20))`, () => {
+  it(`returns Left(NOT_UNEQUAL_TO error) when the value equals the test value`, () => {
     expect(notEqualTo(Just(20))(Just(20))).toEqual(
       Left({
         failures: [{ errorType: undefined, testValue: Just(20) }],
@@ -17,11 +17,11 @@ describe('baseValidators:notEqualTo', () => {
     )
   })
 
-  it(`returns Right(Just(21)) for (Just(20))(Just(21))`, () => {
+  it(`returns Right(Just(value)) when the value is greater than the test value`, () => {
     expect(notEqualTo(Just(20))(Just(21))).toEqual(Right(Just(21)))
   })
 
-  it(`returns Right(Nothing) for (Just(20))(Nothing)`, () => {
+  it(`returns Right(Nothing) when the value is Nothing`, () => {
     expect(notEqualTo(Just(20))(Nothing)).toEqual(Right(Nothing))
   })
 })
